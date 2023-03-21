fac3c322bc6f9056c543bc571aa7e75b4f027c1d
chenwenqian <wenqianchen@ufl.edu>
2023-03-21 14:42:40 -0400

    encode and decode function

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 # Aidan Boudreau & Wen Qian Chen
 # Define the main function
+def encode(password):
+    return password + "555"
+
+def decode(encode_password):
+    original_password = encode_password[:-3]
+    return original_password
 def main():
     while True:
         print("Menu")
@@ -11,11 +17,19 @@ def main():
         option = input("Please enter an option: ")
 
         if option == "1":
-            encode_password()
+            password = input("Please enter your password to encode: ")
+            encode_password = encode(password)
+            print("Your password has been encoded and stored!")
+
         elif option == "2":
-            decode_password()
+            encide_password = input("Please enter the encoded password: ")
+            original_password = decode(encode_password)
+            print("The encoded password is " + encoded_password + ", and the original_password is " + original_password + ".")
+
         elif option == "3":
             print("Goodbye!")
             break
         else:
             print("Invalid option. Please try again.")
+if __name__ == '__main__':
+    main()
